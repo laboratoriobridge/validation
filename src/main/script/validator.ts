53bba40e9d90298736fbda91935da4ed6b099b8e
Carlos Bonetti <carlosb_bc@hotmail.com>
2018-04-16 11:56:28 -0300

    createValidator passing down allValues object to rule functions

--- src/main/script/validator.ts
+++ src/main/script/validator.ts
@@ -1,34 +1,35 @@
-export type RuleFunction = (value: any) => any
+export type ValidatorFunction = (allValues: object) => any
+export type RuleFunction = (value: any, allValues: object) => any
 export interface RuleNestedArray {
     [index: number]: RuleFunction | RuleNestedArray
 }
-export type Rule = RuleFunction | RuleNestedArray
-export type Rules = { [key in string]: Rule }
-export type ValidateFunction = (value: any, errors: any) => any
+export type RuleDefinition = RuleFunction | RuleNestedArray
+export type RuleMap = { [key in string]: RuleDefinition }
+export type ValidateFunction = (value: any, propertiesErrors: object) => object
 
-export function createValidator(rules: Rules, validator?: ValidateFunction): RuleFunction {
-    return (value: any): any => {
+export function createValidator(rules: RuleMap, validator?: ValidateFunction): ValidatorFunction {
+    return (allValues: object): any => {
         const errors = {}
         Object.keys(rules).forEach((key) => {
-            const error = validate(value && value[key], rules[key])
+            const error = validate(rules[key], allValues && allValues[key], allValues)
             if (error) {
                 errors[key] = error
             }
         })
 
         if (validator) {
-            Object.assign(errors, validator(value, errors))
+            Object.assign(errors, validator(allValues, errors))
         }
 
         return Object.keys(errors).length > 0 ? errors : undefined
     }
 }
 
-export function validate(value: any, rule: Rule): any {
+export function validate(rule: RuleDefinition, value: any, allValues: object = {}): any {
     const ruleArray = [].concat.apply([], [].concat(rule)).filter(r => typeof r === 'function')
-    const joinedRules = join(ruleArray)
-    return joinedRules(value)
+    const composedFunction = composeRules(ruleArray)
+    return composedFunction(value, allValues)
 }
 
-const join = (rules) => (value) =>
-    rules.map(rule => rule(value)).filter(error => !!error)[0 /* retorna apenas o primero erro */]
+const composeRules = (rules: RuleFunction[]) => (value, allValues) =>
+    rules.map(rule => rule(value, allValues)).filter(error => !!error)[0 /* retorna apenas o primero erro */]
