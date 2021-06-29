72cbea7c2b2c06e66fe100932bf8ed12dce01a79
zDudaHang <maria.eduarda.melo.hang@gmail.com>
2021-06-29 00:12:10 -0300

    Creating a util class for html tag pattern

--- src/main/kotlin/br/ufsc/bridge/platform/validation/rules/RichTextMaxLengthRule.kt
+++ src/main/kotlin/br/ufsc/bridge/platform/validation/rules/RichTextMaxLengthRule.kt
@@ -1,15 +1,14 @@
 package br.ufsc.bridge.platform.validation.rules
 
 import br.ufsc.bridge.platform.validation.engine.Rule
+import br.ufsc.bridge.platform.validation.util.RegexUtil
 import br.ufsc.bridge.platform.validation.util.Util
 import org.apache.commons.text.StringEscapeUtils
 
 class RichTextMaxLengthRule(private val maxLength: Int) : Rule<String?> {
 
-    private val htmlTagPattern = Regex("\\<.*?\\>|\n")
-
     override fun validate(value: String?): String? {
-        val text = StringEscapeUtils.unescapeHtml4(value?.replace(htmlTagPattern, ""))
+        val text = StringEscapeUtils.unescapeHtml4(value?.replace(RegexUtil.HTML_TAG_PATTERN, ""))
         return if (!Util.isEmpty(text) && text?.length!! > maxLength) "Deve possuir no máximo $maxLength caracteres" else null
     }
 }
