56d8b8f2b522500a25deb8d1d601de8da108d6fa
zDudaHang <maria.eduarda.melo.hang@gmail.com>
2021-07-21 16:32:34 -0300

    Removing unnecessary operators

--- src/main/kotlin/br/ufsc/bridge/platform/validation/rules/RichtTextMinLengthRule.kt
+++ src/main/kotlin/br/ufsc/bridge/platform/validation/rules/RichtTextMinLengthRule.kt
@@ -9,7 +9,7 @@ class RichtTextMinLengthRule(private val minLength: Int) : Rule<String?> {
 
     override fun validate(value: String?): String? {
         val text = StringEscapeUtils.unescapeHtml4(value?.replace(RegexUtil.HTML_TAG_PATTERN, ""))
-        return if (Util.isEmpty(text) || text?.length!! < minLength) {
+        return if (Util.isEmpty(text) || text.length < minLength) {
             "Deve possuir no mínimo $minLength caractere(s)"
         } else null
     }
